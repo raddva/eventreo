022755ecb0b7a340e7416e35c54502a87a58fec5
raddva <111165508+raddva@users.noreply.github.com>
2025-08-09 22:47:32 +0700

    feat: fix for deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <PageHead />
-      <Button>Halooow</Button>
+      <Button color="primary">Halooow</Button>
     </main>
   );
 }
