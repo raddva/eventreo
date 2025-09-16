285f60763a93ecf834b843ca51d73b8e955bd93f
raddva <111165508+raddva@users.noreply.github.com>
2025-09-16 22:38:54 +0700

    feat: Search Event

--- src/components/layouts/LandingPageLayout/LandingPageLayoutNavbar/LandingPageLayoutNavbar.tsx
+++ src/components/layouts/LandingPageLayout/LandingPageLayoutNavbar/LandingPageLayoutNavbar.tsx
@@ -8,14 +8,23 @@ import { Input } from "@heroui/input";
 import { CiSearch } from "react-icons/ci";
 import { Button, ButtonProps } from "@heroui/button";
 import { signOut, useSession } from "next-auth/react";
-import { Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@heroui/react";
+import { Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Listbox, ListboxItem, Spinner } from "@heroui/react";
 import useLandingPageLayoutNavbar from "./useLandingPageLayoutNavbar";
 import { Fragment } from "react";
+import { IEvent } from "@/types/Event";
 
 const LandingPageLayoutNavbar = () => {
     const router = useRouter();
     const session = useSession();
-    const { profileData } = useLandingPageLayoutNavbar();
+    const {
+        profileData,
+        dataEventsSearch,
+        isLoadingEventsSearch,
+        isRefetchingEventsSearch,
+        handleSearch,
+        search,
+        setSearch
+    } = useLandingPageLayoutNavbar();
 
     return (
         <Navbar maxWidth="2xl" isBordered isBlurred={false} shouldHideOnScroll>
@@ -47,8 +56,28 @@ const LandingPageLayoutNavbar = () => {
                         className="w-[300px]"
                         placeholder="Search Event"
                         startContent={<CiSearch />}
-                        onClear={() => { }}
-                        onChange={() => { }} />
+                        onClear={() => setSearch("")}
+                        onChange={handleSearch} />
+                    {search !== "" && (
+                        <Listbox
+                            items={dataEventsSearch?.data || []}
+                            className="absolute right-0 top-12 rounded-xl border bg-white">
+                            {isRefetchingEventsSearch && !isLoadingEventsSearch ? (
+                                (item: IEvent) => (
+                                    <ListboxItem key={item._id} href={`/event/${item.slug}`} className="flex items-center gap-2">
+                                        <Image src={`${item.banner}`} alt={`${item.name}`} className="w-2/5 rounded-md" width={100} height={40} />
+                                        <p className="line-clamp-2 w-3/5 text-wrap">
+                                            {item.name}
+                                        </p>
+                                    </ListboxItem>
+                                )
+                            ) : (
+                                <ListboxItem key="loading">
+                                    <Spinner color="primary" size="sm" />
+                                </ListboxItem>
+                            )}
+                        </Listbox>
+                    )}
                 </NavbarItem>
                 {session.status === "authenticated" ? (
                     <NavbarItem className="hidden lg:block">
--- src/components/layouts/LandingPageLayout/LandingPageLayoutNavbar/useLandingPageLayoutNavbar.ts
+++ src/components/layouts/LandingPageLayout/LandingPageLayoutNavbar/useLandingPageLayoutNavbar.ts
@@ -1,9 +1,15 @@
+import { DELAY, LIMIT_BANNER, PAGE_DEFAULT } from "@/constants/list.constants";
+import useDebounce from "@/hooks/useDebounce";
 import authServices from "@/services/auth.service";
+import eventServices from "@/services/event.service";
 import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/router";
+import { ChangeEvent, useState } from "react";
 
 const useLandingPageLayoutNavbar = () => {
   const router = useRouter();
+  const [search, setSearch] = useState("");
+  const debounce = useDebounce();
   const getProfile = async () => {
     const { data } = await authServices.getProfile();
     return data.data;
@@ -15,8 +21,36 @@ const useLandingPageLayoutNavbar = () => {
     enabled: router.isReady,
   });
 
+  const getEventsSearch = async () => {
+    const params = `search=${search}&limit=${LIMIT_BANNER}&page=${PAGE_DEFAULT}&isPublish=true`;
+    const res = await eventServices.getEvents(params);
+    const { data } = res;
+    return data;
+  };
+
+  const {
+    data: dataEventsSearch,
+    isLoading: isLoadingEventsSearch,
+    isRefetching: isRefetchingEventsSearch,
+  } = useQuery({
+    queryKey: ["EventsSearch", search],
+    queryFn: getEventsSearch,
+    enabled: !!search,
+  });
+
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    debounce(() => setSearch(e.target.value), DELAY);
+  };
+
   return {
     profileData,
+
+    dataEventsSearch,
+    isLoadingEventsSearch,
+    isRefetchingEventsSearch,
+    handleSearch,
+    search,
+    setSearch,
   };
 };
 
